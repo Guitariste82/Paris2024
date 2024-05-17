0fac5ce07a013545c272fe1ad702080907b8a10b
Guitariste82 <13656418+Guitariste82@users.noreply.github.com>
2024-05-17 14:04:24 +0200

    Add Model offer & offerType

--- Models/Offer.cs
+++ Models/Offer.cs
@@ -1,6 +1,34 @@
-﻿namespace Paris2024.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Paris2024.Models;
+
+[Table("Offer")]
+public class Offer
 {
-    public class Offer
-    {
-    }
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string? Offer_Code { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string? Offer_Sport { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string? Offer_Description { get; set; }
+
+    public string? Offer_ImagePath { get; set; }
+
+    [Required]
+    public double Offer_UnitPrice { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(OfferType))]
+    public int OfferTypeId { get; set; }
+    public OfferType OfferType { get; set; }
+
 }
--- Models/OfferType.cs
+++ Models/OfferType.cs
@@ -1,6 +1,17 @@
-﻿namespace Paris2024.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Paris2024.Models;
+
+[Table("OfferType")]
+public class OfferType
 {
-    public class OfferType
-    {
-    }
+    [Required]
+    [MaxLength(20)]
+    [Display(Name = "Type d'offres")]
+    public string OfferType_Name { get; set; }
+
+    public int OfferType_AllowedPerson { get; set; }
+    public virtual ICollection<Offer> Offers { get; set; } // Modif + de flexibilité (type générique)
+    //public List<Offer> Offers { get; set; }
 }
