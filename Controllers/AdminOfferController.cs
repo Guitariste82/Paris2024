282330e9ebc10b8afdedd840c1bf5b19d0b0fe1a
Guitariste82 <13656418+Guitariste82@users.noreply.github.com>
2024-05-17 15:59:30 +0200

    add adminOfferController

---
+++ Controllers/AdminOfferController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Paris2024.Data;
+using Paris2024.Models;
+
+namespace Paris2024.Controllers
+{
+    public class AdminOfferController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminOfferController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AdminOffer
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Offers.Include(o => o.OfferType);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: AdminOffer/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var offer = await _context.Offers
+                .Include(o => o.OfferType)
+                .FirstOrDefaultAsync(m => m.OfferId == id);
+            if (offer == null)
+            {
+                return NotFound();
+            }
+
+            return View(offer);
+        }
+
+        // GET: AdminOffer/Create
+        public IActionResult Create()
+        {
+            ViewData["OfferTypeId"] = new SelectList(_context.OfferTypes, "OfferTypeId", "OfferType_Name");
+            return View();
+        }
+
+        // POST: AdminOffer/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("OfferId,Offer_Code,Offer_Sport,Offer_Description,Offer_ImagePath,Offer_UnitPrice,OfferTypeId")] Offer offer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(offer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OfferTypeId"] = new SelectList(_context.OfferTypes, "OfferTypeId", "OfferType_Name", offer.OfferTypeId);
+            return View(offer);
+        }
+
+        // GET: AdminOffer/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var offer = await _context.Offers.FindAsync(id);
+            if (offer == null)
+            {
+                return NotFound();
+            }
+            ViewData["OfferTypeId"] = new SelectList(_context.OfferTypes, "OfferTypeId", "OfferType_Name", offer.OfferTypeId);
+            return View(offer);
+        }
+
+        // POST: AdminOffer/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("OfferId,Offer_Code,Offer_Sport,Offer_Description,Offer_ImagePath,Offer_UnitPrice,OfferTypeId")] Offer offer)
+        {
+            if (id != offer.OfferId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(offer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OfferExists(offer.OfferId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OfferTypeId"] = new SelectList(_context.OfferTypes, "OfferTypeId", "OfferType_Name", offer.OfferTypeId);
+            return View(offer);
+        }
+
+        // GET: AdminOffer/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var offer = await _context.Offers
+                .Include(o => o.OfferType)
+                .FirstOrDefaultAsync(m => m.OfferId == id);
+            if (offer == null)
+            {
+                return NotFound();
+            }
+
+            return View(offer);
+        }
+
+        // POST: AdminOffer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var offer = await _context.Offers.FindAsync(id);
+            if (offer != null)
+            {
+                _context.Offers.Remove(offer);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OfferExists(int id)
+        {
+            return _context.Offers.Any(e => e.OfferId == id);
+        }
+    }
+}
