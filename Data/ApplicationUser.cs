2b2f88c3b5b5b5a6325e933539fa1754e439c8e2
Guitariste82 <13656418+Guitariste82@users.noreply.github.com>
2024-05-23 21:34:37 +0200

    Add Extra Class For Identity

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Paris2024.Data;
+
+/// <summary>
+ /// Inherit from Identity
+ /// </summary>
+public class ApplicationUser : IdentityUser
+{
+    //FH - add two extented field in dbo.AspNetUsers
+
+    [Required]
+    [StringLength(40, ErrorMessage = "Le prénom ne peut pas dépasser 40 caractères.")]
+    [Display(Name = "Prénom")]
+    public string? FirstName { get; set; }
+
+    [Required]
+    [StringLength(40, ErrorMessage = "Le nom ne peut pas dépasser 40 caractères.")]
+    [Display(Name = "Nom")]
+    public string? LastName { get; set; }
+}
