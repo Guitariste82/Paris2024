145bd219aa2de9c07d0d9c5877c6be02429aa075
Guitariste82 <13656418+Guitariste82@users.noreply.github.com>
2024-05-26 16:48:34 +0200

    Update design for UserOrders & Tickets

--- Repositories/UserOrderRepository.cs
+++ Repositories/UserOrderRepository.cs
@@ -49,16 +49,16 @@ public class UserOrderRepository : IUserOrderRepository
     }
 
 
-    public async Task<OrderItem> TicketsDetails(string? qrcodeKey)
+    public async Task<OrderItem> TicketsDetails(string? TicketID)
     {
-        if (qrcodeKey == null)
+        if (TicketID == null)
         {
             throw new Exception("User is not logged-in");
         }
 
         var OrderDetail = await _context.OrderItems
               .Include(s => s.Offer)
-              .FirstOrDefaultAsync(m => m.OrderItem_QrCode == qrcodeKey);
+              .FirstOrDefaultAsync(m => m.OrderItem_QrCode == TicketID);
 
         if (OrderDetail == null)
         {
